38d7fb1364f1c945adfdc5ae28aac5ec92ab93d5
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-10-27 23:39:12 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -12,12 +12,14 @@ done
 patch -uR ~/.zprezto/runcoms/zpreztorc < ~/dotfiles/zsh/zprezto/zpreztorc.patch
 patch -uR ~/.zprezto/modules/prompt/external/pure/pure.zsh < ~/dotfiles/zsh/zprezto/pure.patch
 
-cp dotfiles/vim/.vimrc ~/.vimrc
-cp -r dotfiles/vim/.vim/ ~/
-sh dotfiles/vim/install.sh
+cp ~/dotfiles/vim/.vimrc ~/.vimrc
+cp -r ~/dotfiles/vim/.vim/ ~/
+sh ~/dotfiles/vim/install.sh
+
+cd
 
 wget https://github.com/peco/peco/releases/download/v0.2.9/peco_linux_amd64.tar.gz
 
-sudo tar -C /usr/local/bin/ -xzf peco_linux_amd64.tar.gz
+tar -xzf peco_linux_amd64.tar.gz
 
 exec zsh -l
