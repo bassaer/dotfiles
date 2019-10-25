50676c0312b6670a80e771f4931702c8ee79ca76
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-10-25 10:36:05 +0900

    Update vim8.sh

--- vim8.sh
+++ vim8.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 set -e
+
 sudo yum -y remove vim-enhanced
 rm -rf /tmp/vim
+
+sudo yum -y install ncurses-devel
 git clone https://github.com/vim/vim.git /tmp/vim
 cd /tmp/vim/src
 sudo make install
+
 vim --version | head -1
