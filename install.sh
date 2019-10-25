d2c84b60b16878e620e5272a29a08c763dd4534e
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-10-25 11:25:17 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
 set -e
-sudo yum -y install zsh git
+
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y remove git git-\*
+
+sudo yum -y install zsh git2u
 chsh -s $(which zsh)
